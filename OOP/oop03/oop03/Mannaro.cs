e80d1abe46892764dc8f780cfca62ad0ff428945
Stud. Emanuele Cucchietti <e.cucchietti.1732@vallauri.net>
2021-10-30 10:58:01 +0200

    OOP 03

---
+++ OOP/oop03/oop03/Mannaro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop03
+{
+    class Mannaro : Mostro
+    {
+        public double punti;
+        public Mannaro(string n)
+        {
+            nome = n;
+            punti = 500;
+        }
+        public double Valore_Proprio()
+        {
+            return punti;
+        }
+
+        public double Valore_Padre()
+        {
+            return base.punti;
+        }
+        public double Valore()
+        {
+            return punti += base.Valore();
+        }
+    }
+}
---
+++ OOP/oop03/oop03/Mostro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop03
+{
+    class Mostro
+    {
+        public string nome;
+        public double punti;
+        public Mostro()
+        {
+            nome = "SenzaNome";
+            punti = 100;
+        }
+        public Mostro(string n)
+        {
+            nome = n;
+            punti = 100;
+        }
+        public double Valore()
+        {
+            return punti;
+        }
+    }
+}
