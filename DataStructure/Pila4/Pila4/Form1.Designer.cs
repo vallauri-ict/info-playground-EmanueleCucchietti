dd43bec7ae3d9c00cc8a4791a4da4d530227eba5
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2021-11-19 20:33:58 +0100

    DataStructure Pile4

---
+++ DataStructure/Pila4/Pila4/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Pila4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodice = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTara = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.loadContainer = new System.Windows.Forms.Button();
+            this.removeContainer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codice";
+            // 
+            // txtCodice
+            // 
+            this.txtCodice.Location = new System.Drawing.Point(58, 6);
+            this.txtCodice.Name = "txtCodice";
+            this.txtCodice.Size = new System.Drawing.Size(100, 20);
+            this.txtCodice.TabIndex = 1;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(58, 32);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Peso";
+            // 
+            // txtTara
+            // 
+            this.txtTara.Location = new System.Drawing.Point(58, 58);
+            this.txtTara.Name = "txtTara";
+            this.txtTara.Size = new System.Drawing.Size(100, 20);
+            this.txtTara.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tara";
+            // 
+            // loadContainer
+            // 
+            this.loadContainer.Location = new System.Drawing.Point(213, 6);
+            this.loadContainer.Name = "loadContainer";
+            this.loadContainer.Size = new System.Drawing.Size(102, 23);
+            this.loadContainer.TabIndex = 6;
+            this.loadContainer.Text = "Carica Container";
+            this.loadContainer.UseVisualStyleBackColor = true;
+            this.loadContainer.Click += new System.EventHandler(this.loadContainer_Click);
+            // 
+            // removeContainer
+            // 
+            this.removeContainer.Location = new System.Drawing.Point(213, 55);
+            this.removeContainer.Name = "removeContainer";
+            this.removeContainer.Size = new System.Drawing.Size(102, 23);
+            this.removeContainer.TabIndex = 7;
+            this.removeContainer.Text = "Estrai Container";
+            this.removeContainer.UseVisualStyleBackColor = true;
+            this.removeContainer.Click += new System.EventHandler(this.removeContainer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.removeContainer);
+            this.Controls.Add(this.loadContainer);
+            this.Controls.Add(this.txtTara);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCodice);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodice;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTara;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button loadContainer;
+        private System.Windows.Forms.Button removeContainer;
+    }
+}
+
---
+++ DataStructure/Pila4/Pila4/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pila4
+{
+    public partial class Form1 : Form
+    {
+        public struct container
+        {
+            public string codice;
+            public int peso;
+            public int tara;
+        }
+        Stack<container> containers = new Stack<container>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void loadContainer_Click(object sender, EventArgs e)
+        {
+            inserisciContainer();
+            clearFields();
+        }
+        private void clearFields()
+        {
+            txtCodice.Text = "";
+            txtPeso.Text = "";
+            txtTara.Text = "";
+        }
+        private void inserisciContainer()
+        {
+            container cnt = new container();
+            cnt.codice = txtCodice.Text;
+            cnt.peso = Convert.ToInt32(txtPeso.Text);
+            cnt.tara = Convert.ToInt32(txtTara.Text);
+            containers.Push(cnt);
+        }
+        private void removeContainer_Click(object sender, EventArgs e)
+        {
+            if (containers.Count != 0)
+            {
+                container cnt;
+                cnt = containers.Pop();
+                MessageBox.Show(
+                    "Il container prelevato è: \n"
+                    +
+                    "Codice: " + cnt.codice + "\n"
+                    +
+                    "Peso: " + cnt.peso + "\n"
+                    +
+                    "Tara: " + cnt.tara + "\n"
+                    );
+            }
+            else
+                MessageBox.Show("Stack vuoto");
+        }
+    }
+}
