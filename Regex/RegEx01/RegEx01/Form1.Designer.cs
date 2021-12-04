6cf7384c13cc835f17373b22ba62dff84a178a82
Stud. Emanuele Cucchietti <e.cucchietti.1732@vallauri.net>
2021-12-04 11:02:41 +0100

    Regex Ex 01

---
+++ Regex/RegEx01/RegEx01/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace RegEx01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtStr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMatch = new System.Windows.Forms.Button();
+            this.txtStr2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(118, 10);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(101, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Verifica Regex";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtStr
+            // 
+            this.txtStr.Location = new System.Drawing.Point(12, 12);
+            this.txtStr.Name = "txtStr";
+            this.txtStr.Size = new System.Drawing.Size(100, 20);
+            this.txtStr.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Uso di label nella stringa: ";
+            // 
+            // btnMatch
+            // 
+            this.btnMatch.Location = new System.Drawing.Point(594, 118);
+            this.btnMatch.Name = "btnMatch";
+            this.btnMatch.Size = new System.Drawing.Size(75, 23);
+            this.btnMatch.TabIndex = 3;
+            this.btnMatch.Text = "button1";
+            this.btnMatch.UseVisualStyleBackColor = true;
+            this.btnMatch.Click += new System.EventHandler(this.btnMatch_Click);
+            // 
+            // txtStr2
+            // 
+            this.txtStr2.AutoSize = true;
+            this.txtStr2.Location = new System.Drawing.Point(476, 12);
+            this.txtStr2.Name = "txtStr2";
+            this.txtStr2.Size = new System.Drawing.Size(35, 13);
+            this.txtStr2.TabIndex = 4;
+            this.txtStr2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStr2);
+            this.Controls.Add(this.btnMatch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStr);
+            this.Controls.Add(this.btnTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox txtStr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMatch;
+        private System.Windows.Forms.Label txtStr2;
+    }
+}
+
