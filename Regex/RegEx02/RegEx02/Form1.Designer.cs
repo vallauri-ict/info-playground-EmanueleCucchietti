ea1f9ccc49a6a67cfaf3b4df87e2fa0aa259c57d
Stud. Emanuele Cucchietti <e.cucchietti.1732@vallauri.net>
2021-12-11 10:16:21 +0100

    Regex Ex 02

---
+++ Regex/RegEx02/RegEx02/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace RegEx02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome: ";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(59, 22);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(342, 22);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Invia";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(59, 48);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(100, 20);
+            this.txtCognome.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome: ";
+            // 
+            // txtEta
+            // 
+            this.txtEta.Location = new System.Drawing.Point(59, 74);
+            this.txtEta.Name = "txtEta";
+            this.txtEta.Size = new System.Drawing.Size(100, 20);
+            this.txtEta.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Età:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCognome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEta;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Regex/RegEx02/RegEx02/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace RegEx02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (verifyNameRegex())
+                MessageBox.Show("Name Corretto");
+            else
+                MessageBox.Show("Name Errato");
+
+            if (verifyEtaRegex())
+                MessageBox.Show("eta Corretto");
+            else
+                MessageBox.Show("eta Errato");
+
+
+        }
+
+        private bool verifyEtaRegex()
+        {
+            string number = " ^[0-9]$";
+            Regex reg = new Regex(number);
+            return (reg.IsMatch(txtNome.Text) && Convert.ToInt32(reg)>= 0 && Convert.ToInt32(reg) <= 120);
+        }
+
+        private bool verifyNameRegex()
+        {
+            string name = "^[a-zA-Z]{3}$";
+            Regex reg = new Regex(name);
+            return reg.IsMatch(txtNome.Text);
+        }
+        private bool verifySurnameRegex()
+        {
+            string name = "^[a-zA-Z]{3}$";
+            Regex reg = new Regex(name);
+            return reg.IsMatch(txtNome.Text);
+        }
+    }
+}
