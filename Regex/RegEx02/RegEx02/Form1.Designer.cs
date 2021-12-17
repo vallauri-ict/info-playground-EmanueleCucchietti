08a659bf7544e93eb4c70bfe07c7dbd790466b3f
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2021-12-17 20:50:25 +0100

    Regex Ex 02-1

--- Regex/RegEx02/RegEx02/Form1.Designer.cs
+++ Regex/RegEx02/RegEx02/Form1.Designer.cs
@@ -35,6 +35,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtEta = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.txtCap = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCitta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -48,7 +58,7 @@
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(59, 22);
+            this.txtNome.Location = new System.Drawing.Point(70, 22);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(100, 20);
             this.txtNome.TabIndex = 1;
@@ -65,7 +75,7 @@
             // 
             // txtCognome
             // 
-            this.txtCognome.Location = new System.Drawing.Point(59, 48);
+            this.txtCognome.Location = new System.Drawing.Point(70, 48);
             this.txtCognome.Name = "txtCognome";
             this.txtCognome.Size = new System.Drawing.Size(100, 20);
             this.txtCognome.TabIndex = 4;
@@ -75,13 +85,13 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Nome: ";
+            this.label2.Text = "Cognome: ";
             // 
             // txtEta
             // 
-            this.txtEta.Location = new System.Drawing.Point(59, 74);
+            this.txtEta.Location = new System.Drawing.Point(70, 74);
             this.txtEta.Name = "txtEta";
             this.txtEta.Size = new System.Drawing.Size(100, 20);
             this.txtEta.TabIndex = 6;
@@ -96,11 +106,106 @@
             this.label3.Text = "Età:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // txtCap
+            // 
+            this.txtCap.Location = new System.Drawing.Point(70, 100);
+            this.txtCap.Name = "txtCap";
+            this.txtCap.Size = new System.Drawing.Size(100, 20);
+            this.txtCap.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "CAP:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtCitta
+            // 
+            this.txtCitta.Location = new System.Drawing.Point(70, 126);
+            this.txtCitta.Name = "txtCitta";
+            this.txtCitta.Size = new System.Drawing.Size(100, 20);
+            this.txtCitta.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Città:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(70, 152);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(364, 20);
+            this.txtMail.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Mail:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(70, 178);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 181);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Username:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(70, 204);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 207);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Password:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCitta);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCap);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.txtEta);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtCognome);
@@ -124,6 +229,16 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtEta;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCap;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCitta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- Regex/RegEx02/RegEx02/Form1.cs
+++ Regex/RegEx02/RegEx02/Form1.cs
@@ -21,36 +21,92 @@ namespace RegEx02
         private void btnSubmit_Click(object sender, EventArgs e)
         {
             if (verifyNameRegex())
-                MessageBox.Show("Name Corretto");
+                MessageBox.Show("Nome Corretto");
             else
-                MessageBox.Show("Name Errato");
+                MessageBox.Show("Nome Errato");
+
+            if (verifySurnameRegex())
+                MessageBox.Show("Cognome Corretto");
+            else
+                MessageBox.Show("Cognome Errato");
 
             if (verifyEtaRegex())
-                MessageBox.Show("eta Corretto");
+                MessageBox.Show("Età Corretta");
             else
-                MessageBox.Show("eta Errato");
+                MessageBox.Show("Età Errato");
 
+            if (verifyCapRegex())
+                MessageBox.Show("CAP Corretto");
+            else
+                MessageBox.Show("CAP Errato");
 
+            if (verifyCittaRegex())
+                MessageBox.Show("Città Corretta");
+            else
+                MessageBox.Show("Città Errata");
+
+            if (verifyMailRegex())
+                MessageBox.Show("Mail Corretta");
+            else
+                MessageBox.Show("Mail Errata");
+
+            if (verifyUsernameRegex())
+                MessageBox.Show("Username Corretto");
+            else
+                MessageBox.Show("Username Errato");
+
+            if (verifyPasswordRegex())
+                MessageBox.Show("Password Corretta");
+            else
+                MessageBox.Show("Password Errata");
+        }
+
+        private bool verifyCapRegex()
+        {
+            string cap = "^[0-9]{5}$";
+            Regex reg = new Regex(cap);
+            return reg.IsMatch(txtCap.Text);
         }
 
         private bool verifyEtaRegex()
         {
-            string number = " ^[0-9]$";
+            string number = "^[0-9]+$";
             Regex reg = new Regex(number);
-            return (reg.IsMatch(txtNome.Text) && Convert.ToInt32(reg)>= 0 && Convert.ToInt32(reg) <= 120);
+            return (reg.IsMatch(txtEta.Text) && (Convert.ToInt32(txtEta.Text) >= 0 || Convert.ToInt32(txtEta.Text) <= 120));
         }
 
         private bool verifyNameRegex()
         {
-            string name = "^[a-zA-Z]{3}$";
+            string name = "^[a-zA-Z]{3,}$";
             Regex reg = new Regex(name);
             return reg.IsMatch(txtNome.Text);
         }
         private bool verifySurnameRegex()
         {
-            string name = "^[a-zA-Z]{3}$";
+            string name = "^[a-zA-Z]{3,}$";
             Regex reg = new Regex(name);
-            return reg.IsMatch(txtNome.Text);
+            return reg.IsMatch(txtCognome.Text);
+        }
+        private bool verifyCittaRegex()
+        {
+            string citta = "^[a-zA-Z]{3,} [(a-zA-Z][a-zA-Z)]$";
+            Regex reg = new Regex(citta);
+            return reg.IsMatch(txtCitta.Text);
+        }
+        private bool verifyMailRegex()
+        {
+            Regex reg = new Regex(@"^(\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)(,\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)*$");
+            return reg.IsMatch(txtMail.Text);
+        }
+        private bool verifyUsernameRegex()
+        {
+            Regex reg = new Regex(@"[a-zA-Z]\w{4,}");
+            return reg.IsMatch(txtUsername.Text);
+        }
+        private bool verifyPasswordRegex()
+        {
+            Regex reg = new Regex(@"[a-zA-Z0-9.!]{8,15}||[a-zA-Z0-9.-]{8,15}||[a-zA-Z0-9.+]{8,15}");
+            return reg.IsMatch(txtPassword.Text);
         }
     }
 }
